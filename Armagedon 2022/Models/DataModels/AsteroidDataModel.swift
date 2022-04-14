55f58e8c47c644c08f20ccc634a800859609badd
Fett00 <musaew2014@gmail.com>
2022-04-15 01:24:17 +0300

    Создана модель данных для астероидов

---
+++ Armagedon 2022/Models/DataModels/AsteroidDataModel.swift
@@ -0,0 +1,17 @@
+//
+//  AsteroidDataModel.swift
+//  Armagedon 2022
+//
+//  Created by Садык Мусаев on 15.04.2022.
+//
+
+import Foundation
+
+struct AsteroidDataModel{
+    
+    let name: String
+    let diameter: Int
+    let destinationTime: String
+    let distance: String
+    let isDangerous: Bool
+}
